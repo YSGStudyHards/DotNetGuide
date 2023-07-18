bb33e44095527689272affeebb42b716fe3acecf
Edwin <1070342164@qq.com>
2023-07-18 22:28:03 +0800

    add HelloDotNetGuide

---
+++ src/DotNetGuide/HelloDotNetGuide/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloDotNetGuide
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello DotNetGuide!");
+        }
+    }
+}
