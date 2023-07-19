ceaa28ef05692f72f32de61f054bec71bd8cf2ad
Edwin <1070342164@qq.com>
2023-07-20 00:46:49 +0800

    Add 选择排序算法

--- DotNetGuidePractice/HelloDotNetGuide/常见算法/选择排序算法.cs
+++ DotNetGuidePractice/HelloDotNetGuide/常见算法/选择排序算法.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HelloDotNetGuide.常见算法
+﻿namespace HelloDotNetGuide.常见算法
 {
     public class 选择排序算法
     {
+        //选择排序（Selection Sort）是一种简单的排序算法，其实现原理如下：
+        //1、遍历待排序数组，从第一个元素开始。
+        //2、假设当前遍历的元素为最小值，将其索引保存为最小值索引（minIndex）。
+        //3、在剩余的未排序部分中，找到比当前最小值还要小的元素，并更新最小值索引。
+        //4、在遍历结束后，将找到的最小值与当前遍历位置的元素进行交换。
+        //5、重复步骤2到4，直到排序完成。
+        //选择排序算法的时间复杂度为O(n^2)，其中n是待排序数组的大小。尽管其时间复杂度较高，但选择排序算法比较简单易懂，并且在某些特定情况下，例如对于小规模的数组来说，其性能可能表现得比其他高级排序算法要好。
+
         /// <summary>
         /// 选择排序算法
-        /// 选择排序算法的核心思想是每次从未排序的部分中找到最小元素，然后将其与未排序部分的第一个元素交换位置。
-        /// 通过不断进行这个过程，逐步将最小元素放到正确的位置，完成整个数组的排序。
         /// </summary>
         public static void SelectionSortAlgorithmMain()
         {
-            int[] array = { 64, 25, 12, 22, 11 };
+            int[] array = { 64, 25, 12, 22, 11, 99, 3, 100 };
 
             Console.WriteLine("原始数组: ");
             PrintArray(array);
